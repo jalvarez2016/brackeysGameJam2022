284b9a984e360ef2240221b46757dc31db59c54b
jason alvarez <jasonalvarezcoraiza@gmail.com>
2022-08-23 22:56:32 -0400

    Add player animation play on key press and sprinting logic

--- Player.gd
+++ Player.gd
@@ -1,16 +1,16 @@
 extends KinematicBody2D
 
 const ACCELERATION = 500
-const MAX_SPEED = 120
+const MAX_SPEED = 100
 const FRICTION = 500
 var velocity = Vector2.ZERO
-var SPRINTSPEED = 1
-var ISStrong = false
+var IsStrong = false
+var isSprinting = false
+var money = 0
+var lastKnownDirection = 0
 
 
-func _ready():
-	pass # Replace with function body.
-
+onready var animationPlayer = $AnimationPlayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -20,12 +20,44 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		# Sprinting needs fixing
-		if Input.is_action_pressed("ui_sprint"):
-			SPRINTSPEED = 2
-			print("sprinting!!")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED * SPRINTSPEED, ACCELERATION * delta)
+		lastKnownDirection = input_vector.x
+		#animation logic
+		if input_vector.x > 0:
+			if IsStrong :
+				animationPlayer.play("WalkRightStrong")
+			else:
+				animationPlayer.play("WalkRightNormal")
+		else:
+			if IsStrong :
+				animationPlayer.play("WalkLeftStrong")
+			else:
+				animationPlayer.play("WalkLeftNormal")
+			
+		
+		#sprinting logic
+		isSprinting = Input.is_action_pressed("ui_sprint")
+		if Input.is_action_just_released("ui_sprint"):
+			isSprinting = false
+		
+		if isSprinting:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED * 2, ACCELERATION * delta)
+		else:
+			velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+			
+		
 	else:
+		#animation logic
+		if lastKnownDirection > 0:
+			if IsStrong :
+				animationPlayer.play("IdleRightStrong")
+			else:
+				animationPlayer.play("IdleRightNormal")
+		else:
+			if IsStrong :
+				animationPlayer.play("IdleLeftStrong")
+			else:
+				animationPlayer.play("IdleLeftNormal")
+
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
