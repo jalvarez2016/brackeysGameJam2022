0d1b39b3b9e5bbae85007db7de1744b63fcf58e7
jason alvarez <jasonalvarezcoraiza@gmail.com>
2022-08-25 21:14:36 -0400

    Add enemy follow player

--- Enemies/Zombie Enemy.gd
+++ Enemies/Zombie Enemy.gd
@@ -2,18 +2,37 @@ extends KinematicBody2D
 
 #var state
 var knockback = Vector2.ZERO
+var WALK_SPEED = 80
 onready var playerDetectionZone = $PlayerDetectionZone
+onready var player = get_node("/root/World/YSort/Player")
+
+enum {
+	ATTACK,
+	STAY
+}
+var state = STAY
 
 export(int) var health = 3
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
-
-func seekPlayer():
-	if playerDetectionZone._can_see_Player():
-		print("sees player")
+	var playerVisible = playerDetectionZone._can_see_Player()
+	#print(playerVisible)
+	if playerVisible && state != ATTACK:
+		state = ATTACK
+	else:
+		state = STAY
 		
+	if player:
+		match state:
+			ATTACK:
+				var direction = (player.position - position).normalized()
+				direction = move_and_slide(direction * WALK_SPEED)
+				pass
+			STAY:
+				pass
+
 func _on_Hurtbox_area_entered(area):
 	health -= 1
 	if health <= 0:
--- PlayerDetectionZone.gd
+++ PlayerDetectionZone.gd
@@ -3,11 +3,14 @@ extends Area2D
 var player = null
 
 func _can_see_Player():
-	player != null
-
+	return player != null
+	
 func _on_PlayerDetectionZone_area_exited(area):
 	player = null
+	print("exited")
 
 func _on_PlayerDetectionZone_area_entered(area):
-	print('entered area')
-	player = area
+	if area.find_parent('Player'):
+		player = area.find_parent('Player').position
+		#player.is_in_group("player")
+		print("entered")
